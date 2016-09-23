e4152ee71212b2d460c8d6079b752e52a449e0e3
Abraham Hernandez <abraham@gmail.com>
2016-09-22 19:37:14 -0500

    Agregando Estilos

--- application/views/index_view.php
+++ application/views/index_view.php
@@ -1,30 +1,243 @@
-<!DOCTYPE html>
-<html>
-    <head> 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Inicio</title>
-    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
-    </head> 
-<body>
-    <div class="container">
-        <h1 style="font-size:20pt">Bien</h1>
-        <li><a href="<?= site_url('videos/index') ?>">Videos</a></li>
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta meta charset="utf-8">
+        <meta meta http-equiv="X-UA-Compatible" content="IE=edge">
+	      <title>::: Noticias del Perú y el Mundo :::</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <meta name="language" content="es" />
+        <meta name="bi3dPubDate" content="2016-09-21T23:08:31-05:00" />
+        <meta name="robots" content="index,follow"/>
+        <meta name='googlebot' content='index, follow' />
+        <meta name="organization" content="Empresa ...... S.A." />
+        <link rel="publisher" href=""/>
+        <meta name="locality" content="Lima, Peru" />
+        <meta property="fb:app_id" content="1667917060088448"/>
+        <!-- seo_metas_portadas -->
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/styles.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/base-ui.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/carreraclaro_timer.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/clasicoespanol.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/ec.logo.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/flash_elecciones.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/mod.aec.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/mod.champions.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/mod.ecplay.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/mod.eliminatorias.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/mod.lifweek.block.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/mod.lifweek.css')?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/style/widget_suplementos.css')?>">
+    <!--bluekai--> 	
+	</div>
+
+		<nav class="ec-marcas">
+	<ul>
+		<li><a href="" >Novedades!</a></li>
+		<li><a href="" >¡Vamos!</a></li>
+		<li><a href="" >Ruedas &amp; Tuercas</a></li>
+		<li><a href="" >MenúPerú</a></li>
+		<li><a href="" >Casa y Más</a></li>
+		<li><a href="" target="_blank" >El Dominical</a></li>
+		<li><a href="" target="_blank" >Urbania</a></li>
+		<li><a href="" target="_blank" >Neoauto</a></li>
+		<li><a href="" >Aptitus</a></li>
+	</ul>
+</nav>	
+	<div class="ec-apertura">
+	<!--links toma de canal
+			  <div class="ads-980x180">
+        <script type="text/javascript">eplAD4M("Top");</script><noscript>
+        <a href="" target="_blank"><img width="960" height="90"  alt="" src="" border=0></a></noscript>
     </div>
-
-<script src="<?php echo base_url('assets/jquery/jquery-2.1.4.min.js')?>"></script>
-<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js')?>"></script>
-<script src="<?php echo base_url('assets/datatables/js/jquery.dataTables.min.js')?>"></script>
-
-<script type="text/javascript">
-
-var save_method; //for save method string
-var table;
-
-$(document).ready(function() {
-
-});
-</script>
-</body>
+    <div class="ads-zocalo1"><script type="text/javascript">eplAD4M("Zocalo1");</script></div>
+    <div class="ads-zocalo2"><script type="text/javascript">eplAD4M("Zocalo2");</script></div>      -->  
+<header id="ec-header">
+    <div class="ec-u-opts">
+        <ul>
+            <li id="user-info" class="u-login">
+                <div class="u-links">
+                    <a href="#" class="go-login" >Ingresa</a>
+                    <a href="#" class="go-registrar" >Regístrate</a>
+                </div>							
+            </li>
+            <li class="u-suscribir">
+                <a href="&amp;" class="go-suscribir" target="_blank" >Suscríbete</a>
+            </li>
+                <li class="u-fecha-clima">22 de Septiembre del 2016</li>
+                <li class="u-buscar">
+                <form method="get" action="" onSubmit="gec.fn.buscar(this,event);">
+                    <input type="text" name="b" placeholder="Buscar">
+                    <button type="submit">&#xf002;</button>
+                </form>
+            </li>
+        </ul>
+    </div>
+    <div class="ec-logo-slinks">
+        <!--<a href="" class="go-suscriptores" target="_blank">Club Suscriptores</a>-->
+        <h1><a href="" >Empresa AAA</a></h1>
+        <div class="social-links">
+          <ul>
+            <li class="rss"><a href="" target="_blank" >&#xf09e;</a></li>
+            <li class="fb"><a href="" target="_blank" >&#xf09a;</a></li>
+            <li class="tw"><a href="" >&#xf099;</a></li>
+            <li class="gp"><a href="" target="_blank" >&#xf0d5;</a></li>
+            <li class="yt"><a href="" target="_blank" >&#xf16a;</a></li>
+          </ul>
+        </div>
+    </div>
+</header>
+        <div class="ec-nav" id="ec-nav">
+          <div class="ec-menu-fixed">
+             <nav id="ec-menu">
+                <h2 class="ec-logo">
+                  <a href="&amp;ft=menu_nav" >Empresa AAA</a>
+                </h2>
+                <ul id="ec-menu-items">
+                  <li class="m-home">
+<!--                    <a  class="&amp;ft=menu_nav" >Inicio</a>-->
+                  </li>
+                  <li class="m-politica ">
+                    <a  href="&amp;ft=menu_nav" >Política</a>
+                  </li>
+                  <li class="m-gastronomia ">
+                    <a  href="&amp;ft=menu_nav" >Economía</a>
+                  </li>
+                  <li class="opinion ">
+                    <a  href="&amp;ft=menu_nav" >Opinión</a>
+                  </li>
+                  <li class="peru ">
+                    <a  href="&amp;ft=menu_nav" >Sociedad</a>
+                  </li>
+                  <li class="m-mundo ">
+                    <a  href="&amp;ft=menu_nav" >Mundo</a>
+                  </li>
+                  <li class="tecno ">
+                    <a  href="&amp;ft=menu_nav" >Tecno</a>
+                  </li>
+                  <li class="m-ciencias last-type">
+                    <a  href="&amp;ft=menu_nav" >Ciencias</a>
+                  </li>
+                  <li class="m-luces sb-item">
+                    <a href="" >Luces</a>
+                  </li>
+                  <li class="m-tvmas sb-item">
+                    <a href="" >TV+</a>
+                  </li>
+                  <li class="m-dt sb-item">
+                    <a href="" >DT</a>
+                  </li>
+                  <li class="m-ecplay">
+                    <a href="&amp;ft=menu_follow">Play</a>
+                  </li>
+                  <li class="m-secciones">
+                    <a data-target="#m-subsecciones"  onclick="gec.fn.dd(this,event)" href="#Más+" >+</a>
+                  </li>
+                </ul>
+            </nav>
+          </div>
+        </div>
+</header>        
+      <div class="ec-click">
+          <h3>Click Aquí</h3>
+            <div class="ui-orbit sl-click orbit-ed">
+            <div class="ui-content">
+            <div class="ui-inner" style="width: 300%;">                
+            <div class="ui-item ui-active" style="width: 33.3333%; margin-right: 0%;">
+          <figure>
+            <a href="" class="img">
+              <img src="" width="940" height="525" alt="¿Qué tendencias influyen en el consumidor de ropa masculina?">
+            </a>           
+            <figcaption>
+              <h2><a href="">¿Qué tendencias y Novedades para Caballeros?</a></h2>
+            </figcaption>
+          </figure>
+        <div class="ec-share">
+          <ul class="ui-shcount" data-url="">
+            <li class="fb">
+              <a href="" data-send="false" data-layout="button_count" data-width="110" data-show-faces="false" target="_blank" onclick="gec.fn.popup(this,event)"><i></i> Me gusta
+              </a> 
+              <span><i></i> 
+                <span data-counter="fb">0</span>
+              </span>
+              </li>
+                <li class="tw">
+                  <a href="" data-lang="es" data-related="" data-hashtags="" data-dnt="true" target="_blank" onclick="gec.fn.popup(this,event)"><i></i> Twittear
+                  </a>
+                <span style="display: none;"><i></i> 
+                  <span data-counter="tw">0</span>
+                </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+        <div class="ui-item" style="width: 33.3333%; margin-right: 0%;">
+            <figure>
+              <a href="" class="img">
+                <img src="" width="940" height="525" alt="Pokémon Go: las evoluciones que están esperando los fans">
+              </a>
+              <figcaption>
+                <h2><a href="">Pokémon Go: las evoluciones que están esperando los fans</a></h2>       
+              </figcaption>
+            </figure>
+        <div class="ec-share">
+          <ul class="ui-shcount" data-url="">
+            <li class="fb">
+                <a href="" data-send="false" data-layout="button_count" data-width="110" data-show-faces="false" target="_blank" onclick="gec.fn.popup(this,event)"><i></i> Me gusta</a>
+                <span><i></i> <span data-counter="fb">0</span></span>
+            </li>
+            <li class="tw">
+              <a href="" data-lang="es" data-related="" data-hashtags="" data-dnt="true" target="_blank" onclick="gec.fn.popup(this,event)"><i></i> Twittear</a> <span style="display: none;"><i></i> 
+              <span data-counter="tw">0</span></span>
+            </li>
+          </ul>
+        </div>
+      </div>             
+      <div class="ui-arrows">
+                      <a href="#prev" class="ui-prev"></a>
+                      <a href="#next" class="ui-next"></a>
+      </div>
+      <footer>     
+      <div class="footer-cols">
+        <div class="info-site ec-info">
+          <h2><a href="/"><img alt="" width="180" height="30"></a></h2>
+            <ul class="legal">
+              <li itemtype="http://schema.org/CreativeWork" itemscope="">
+                <large itemprop="copyrightHolder">
+                  <span itemprop="name">© Empresa Editora El Comercio</span>
+                </large>
+              </li>
+              <li>Jr. Avenida Avenida #300 Lima, Perú</li>
+              <li>Copyright© Empresa AAA - Empresa AAA - Todos los derechos reservados.</li>
+            </ul>
+        </div>
+      <div class="ec-mails">
+        <ul>
+          <li>Director General: Pablo Pablo Pablo</li>
+          <li>Director Periodístico: Perico Olaechea</li>
+          <li>Suscripciones: <a href="mailto:suscriptores@comercio.com.pe">suscriptores@EmpresaAAA.com.pe</a></li>
+          <li>Club Empresa AAA: <a href="mailto:clubelcomercio@comercio.com.pe">EmpresaAAA@....com.pe</a></li>
+          <li><a class="last" target="_blank" href="">Compromiso de Autorregulación Comercial</a></li>
+        </ul>
+      </div>
+      <div class="ec-terminos">
+        <ul>
+          <li><a href="">Términos y condiciones de uso</a></li>
+          <li><a target="_blank" href="">Trabaja para Grupo El Comercio</a></li>
+          <li><a target="_blank" href="">Libro de Reclamaciones</a></li>
+          <li><a target="_blank" href="">Oficinas Concesionarias</a></li>
+          <li><a href="">Principios Rectores</a></li>
+          <li><a href="">Políticas de Privacidad</a></li>
+        </ul>
+      </div>
+      <div class="ec-gda">
+          <p>Miembro de<br>
+            Grupo de Diarios América
+          </p>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </footer>       
+  </body>
 </html>
