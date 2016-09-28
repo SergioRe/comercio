12929aa382bc40c3668a3104c62b0b2b7aaa0137
Sergio Renato Mendoza Pisconte <renato.mpisconte@gmail.com>
2016-09-28 10:54:02 -0500

    Subienod estilos

--- application/views/indexx_view.php
+++ application/views/indexx_view.php
@@ -15,22 +15,22 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript" ></script>
     <script src="<?php echo base_url('assets/bootstrap/js/menu.js')?>" type="text/javascript"></script> 
     <link href="<?php echo base_url('assets/bootstrap/css/bootstrap-social.css')?>" rel="stylesheet">
-
+<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js')?>" type="text/javascript"></script> 
 </head>
 <body>
     <div class="mainWrap">
         <a id="touch-menu" class="mobile-menu" href="#"><i class="icon-reorder"></i>Menu</a>
         <nav>
             <ul class="menu">
-                <li><a href="#">Viu!</a></li>
-                <li><a href="#">¡Vamos!</a></li>
-                <li><a href="#">Ruedas</a></li>
-                <li><a href="#">MenúPerú</a></li>
-                <li><a href="#">Casa</a></li>
-                <li><a href="#">El Dominical</a></li>
-                <li><a href="#">Urbania</a></li>
-                <li><a href="#">Neoauto</a></li>
-                <li><a href="#">Aptitus</a></li>
+                <li class="movil"><a href="#">Viu!</a></li>
+                <li class="movil"><a href="#">¡Vamos!</a></li>
+                <li class="movil"><a href="#">Ruedas</a></li>
+                <li class="movil"><a href="#">MenúPerú</a></li>
+                <li class="movil"><a href="#">Casa</a></li>
+                <li class="movil"><a href="#">El Dominical</a></li>
+                <li class="movil"><a href="#">Urbania</a></li>
+                <li class="movil"><a href="#">Neoauto</a></li>
+                <li class="movil"><a href="#">Aptitus</a></li>
             </ul>
         </nav>
     </div><!--end mainWrap-->
@@ -45,25 +45,67 @@
             <div class="col-lg-4 text-center" style="top: 7px;">
                 <?php echo $fecha;?>
                 <br/><br/>
-                <img src="<?php echo base_url('assets/imagenes/ec_logo.png')?>">
+                <img class="imgsus" src="<?php echo base_url('assets/imagenes/ec_logo.png')?>">
+                <br/><br/>
             </div>
             <div class="col-lg-4" style="top: 3px;">
                 <div class="row">
                     <div class="col-lg-6">
                         <img src="<?php echo base_url('assets/imagenes/btn_suscriptores.png')?>">
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-lg-6 pull-left">
                         <div class="input-group">
-                            <input type="text" class="form-control"/>
-                            <span class="input-group-addon">
-                                <i class="fa fa-search"></i>
-                            </span>
+                            <input type="text" class="buscar" placeholder="Buscar"/>
+                            <button type="submit" class="buscarbtn">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </div>
+                        <div class="vg-social">
+                            <ul>
+                                <li class="fb">
+                                    <a class="btn btn-social-icon btn-facebook pull-right"><span class="fa fa-facebook"></span></a>
+                                </li>
+                                <li class="tw">
+                                    <a class="btn btn-social-icon btn-google pull-right"><span class="fa fa-google"></span></a>
+                                </li>
+                                <li class="gp">
+                                    <a class="btn btn-social-icon btn-twitter pull-right"><span class="fa fa-twitter"></span></a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-            <br/><br/><br/><br/>
         </div>
+        <BR/>
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Cambiar Navegacion</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#" class="navbar-brand">CC</a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">POLITICA</a></li>
+                    <li><a href="#">ECONOMIA</a></li>
+                    <li><a href="#">OPINION SOCIEDAD</a></li>
+                    <li><a href="#">MUNDO</a></li>
+                    <li><a href="#">TECNO</a></li>
+                    <li><a href="#">WUF</a></li>
+                    <li><a href="#">LUCES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">DT</a></li>
+                    <li><a href="#">Servicios</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="row"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></div>
     </div>
 </body>
 </html>
